ae2eef1afda530b2eaf7d56b5df6634a7945be73
Anton <bobon@MacBook-Pro.local>
2022-02-18 15:48:58 +0700

    Не установлен модуль для простых чисел, добавил функцию ручной генерации и проверки

--- src/Games/BrainPrime.php
+++ src/Games/BrainPrime.php
@@ -5,17 +5,38 @@ namespace Brain\Games\Prime;
 use Brain\Games\Engine as Engine;
 
 /**
+ * Генерация списка простых
+ * @return int[]
+ */
+function getPrime()
+{
+    $max = 1000000;
+    $primes = [2, 3, 5];
+
+    for ($i = 7; $i < $max; $i += 2) {
+        $sqrt_i = (int)sqrt($i);
+
+        for ($j = 0; $primes[$j] <= $sqrt_i; $j++)
+            if ($i % $primes[$j] == 0) continue 2;
+
+        $primes[] = $i;
+    }
+    return $primes;
+}
+
+
+/**
  * @param $iter количество данных для игры
  * @return array
  */
 function generateData($iter = 3)
 {
-
+    $arrPrime = getPrime();
     $data = [];
     for ($i = 0; $i < $iter; $i++) {
         $val = rand(1, 1000);
         $question = "{$val}";
-        $answer = gmp_prob_prime($question) ? 'yes' : 'no';
+        $answer = in_array($val, $arrPrime) ? 'yes' : 'no';
         $data[] = [$question, $answer];
     }
     return $data;
